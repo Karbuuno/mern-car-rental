b50725a0c91766211aa6f847f8a83610862bee82
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2024-02-25 22:50:54 +0000

    connect to the database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 import path from "path";
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+
+//port
 const port = process.env.PORT || 5000;
+
+// connection to the database
+connectDB();
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
