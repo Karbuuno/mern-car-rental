8552e14c223eaf8e30186f0d6de6662da857df15
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2024-02-26 16:34:05 +0000

    User authentication is done

---
+++ server/controllers/userController.js
@@ -0,0 +1,63 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/UserModel.js";
+
+// @desc    Auth user & get token
+// @route   POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      // token,
+    });
+  } else {
+    res.status(403);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+export { authUser, registerUser };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
 //port
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api/users", userRoutes);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
